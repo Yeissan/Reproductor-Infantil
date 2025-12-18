8149968aba924c21542a5ddfa4b40ce6a78e76b7
Yeissan <Kirasha1213@gmail.com>
2025-12-18 14:08:16 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const audio = document.getElementById('audio');
 
-audio.src = "audio.mp3";
+audio.src = "https://github.com/Yeissan/Musica-Infantil/blob/7aa6c2ce33a9ec37de01c5bd91808ed968b45635/La%20herramienta%20Magica.mp3";
 
 function playSong() {
   audio.play();
